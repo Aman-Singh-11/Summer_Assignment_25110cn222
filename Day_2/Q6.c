c23f841632a55dccbaa8e6fabf9d6b8868196456
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-05-29 22:22:24 +0530

    Updated Q6 code

---
+++ Day_2/Q6.c
@@ -0,0 +1,16 @@
+// WRITE A PROGRAM TO REVERSE A NUMBER
+#include<stdio.h>
+int main (){
+int n,a,b=0;
+printf("Enter a number to find its Reverse : ");
+scanf("%d",&n);
+while(n>0){
+a = n % 10;
+b = b*10+a;
+n = n/10;
+}
+printf("REVERSE OF THAT NUMBER IS : %d", b);
+return 0;
+
+
+}
