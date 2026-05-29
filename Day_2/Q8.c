b4e03a060323cf70e3d728822eccb54595fb8541
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-05-29 22:55:34 +0530

    Added Q8

---
+++ Day_2/Q8.c
@@ -0,0 +1,18 @@
+// WAP TO CHECK WHETHER A NUMBER IS PALINDROME
+#include<stdio.h>
+int main() {
+int n,a,b=0,t;
+printf("Enter a Number to check it is Palindrome or not : ");
+scanf("%d", &n);
+t=n;
+while(n>0){
+a=n%10;
+b=b*10+a;
+n=n/10;
+}
+if(t==b)
+printf("NUMBER IS PALINDROME");
+else
+printf("NUMBER IS NOT PALINDROME");
+return 0;
+} 
