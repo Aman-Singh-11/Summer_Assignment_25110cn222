70a24faf482a3ef886e552cd8cd412d32f129254
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-05-29 22:38:40 +0530

    Added Q7

---
+++ Day_2/Q7.c
@@ -0,0 +1,14 @@
+// WRITE A PROGRAM TO FIND PRODUCT OF DIGITS
+#include<stdio.h>
+int main(){
+int n,a,b=1;
+printf("Enter a Number to get its digits product : ");
+scanf("%d", &n);
+while(n>0){
+a=n%10;
+b=b*a;
+n=n/10;
+}
+printf("The product of digits = %d", b);
+return 0;
+}
