32d7bfcdae4f719bea5414262ea8af5175fd6ca2
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-06 23:10:39 +0530

    Uploaded 24

---
+++ Day_6/Q24.c
@@ -0,0 +1,17 @@
+//WAP TO FIND X^n without pow()
+#include <stdio.h>
+
+int main() {
+
+    int x, n, i;
+    long long result = 1;
+    printf("Enter base: ");
+    scanf("%d", &x);
+    printf("Enter power: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++) {
+    result = result * x;
+    }
+    printf("%d^%d = %lld", x, n, result);
+    return 0;
+}
