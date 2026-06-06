12444dc24ddfe4fb311cb93cbf46d5b336295aaa
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-06 23:07:10 +0530

    Uploaded 23

---
+++ Day_6/Q23.c
@@ -0,0 +1,16 @@
+//WAP TO COUNT SET BITS IN A NUMBER 
+#include <stdio.h>
+
+int main() {
+int n, count = 0;
+printf("Enter a number: ");
+scanf("%d", &n);
+while(n > 0) {
+if(n % 2 == 1) {
+count++;
+}
+n = n / 2;
+}
+printf("Number of set bits = %d", count);
+return 0;
+}
