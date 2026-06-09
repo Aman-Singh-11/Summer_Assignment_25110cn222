a76e76b80d2814d74884e45bc90e0ea8370daa48
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-09 22:33:31 +0530

    Uploaded 26

--- Day_7/Q25.c
+++ Day_7/Q25.c
@@ -1,16 +1,26 @@
-//WAP TO RECURSIVE FACTORIAL
+//WAP TO RECURSIVE FIBONACCI
 #include <stdio.h>
-long int factorial(int n){
-if (n == 0 || n == 1)
+int fibonacci(int n){
+if(n == 0)
+return 0;
+if(n == 1)
 return 1;
-else
-return n * factorial(n - 1);
+return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main(){
-int num;
-printf("Enter a number: ");
-scanf("%d", &num);
-printf("Factorial of %d = %ld\n", num, factorial(num));
-return 0;
+int main()
+{
+    int n, i;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
 }
