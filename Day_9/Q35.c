052bec61c11e14dd5072cdc12b95a0f5b6db508b
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-09 23:17:11 +0530

    Uploaded 35

---
+++ Day_9/Q35.c
@@ -0,0 +1,18 @@
+/* WAP TO PRINT REPEATED CHARACTER PATTERN    A
+                                              BB
+                                              CCC
+                                              DDDD
+                                              EEEEE */
+#include <stdio.h>
+
+int main()
+{
+int i, j;
+for(i = 1; i <= 5; i++){
+for(j = 1; j <= i; j++){
+printf("%c", i + 64);
+}
+printf("\n");
+}
+return 0;
+}
