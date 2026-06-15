25812e272167a2fe0c82f3f39075fe0d41136c6a
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-15 17:38:10 +0530

    Added Q58

---
+++ Day_15/Q58.c
@@ -0,0 +1,22 @@
+// WAP TO ROTATE ARRAY LEFT.
+#include<stdio.h>
+int main (){
+int arr[100],n,i,temp;
+printf("Enter the size of the array : ");
+scanf("%d", &n);
+printf("Enter array element : ");
+for(i=0;i<n;i++){
+scanf("%d", &arr[i]);
+}
+temp=arr[0];
+for(i=0;i<n-1;i++){
+arr[i]=arr[i+1];
+}
+arr[n-1]=temp;
+printf("Array after left rotation = { ");
+for(i=0;i<n;i++){
+printf("%d ",arr[i]);
+}
+printf("}");
+return 0;
+}
