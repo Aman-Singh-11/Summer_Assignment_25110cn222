fa382dacfba5eafa0352289cbb1593dcba2fc169
Aman-Singh-11 <amansinghutras.3434@gmail.com>
2026-06-27 22:03:42 +0530

    ADDED  Q101

---
+++ Day_26/Q101.c
@@ -0,0 +1,31 @@
+// WAP TO CREATE NUMBER GUESSING GAME.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+int number, guess, attempts = 0;
+srand(time(0));
+number = rand() % 100 + 1;
+printf("===== NUMBER GUESSING GAME =====\n");
+printf("Guess a number between 1 and 100\n");
+do
+{
+printf("Enter your guess: ");
+scanf("%d", &guess);
+attempts++;
+if (guess > number){
+printf("Too High!\n");
+}
+else if (guess < number)
+{
+printf("Too Low!\n");
+}
+else
+{
+printf("Congratulations! You guessed the correct number.\n");
+printf("Total Attempts = %d\n", attempts);
+}
+} while (guess != number);
+return 0;
+}
